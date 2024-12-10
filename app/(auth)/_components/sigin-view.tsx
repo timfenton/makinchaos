31f6d35a53df410f58a977964ab928fb378cface
Tim Fenton <timothyfenton@outlook.com>
2024-12-09 20:43:09 -0500

    signin page changes

--- app/(auth)/_components/sigin-view.tsx
+++ app/(auth)/_components/sigin-view.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import UserAuthForm from './user-auth-form';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import ChaosLogo from '@/app/(user app)/_components/logo';
 
 export const metadata: Metadata = {
   title: 'Authentication',
@@ -24,28 +25,15 @@ export default function SignInViewPage() {
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-medium">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
-          Logo
+          <ChaosLogo linkHref='/signin' />
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
             <p className="text-lg">
-              &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than ever
-              before.&rdquo;
+              Welcome to www.makinchaos.com! </p>
+            <p>Here you can claim your chaos or place a custom order. <br />Just signin with Facebook and submit the form.
             </p>
-            <footer className="text-sm">Sofia Davis</footer>
+            <footer className="text-sm">- Trina</footer>
           </blockquote>
         </div>
       </div>
