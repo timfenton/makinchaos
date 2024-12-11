48bee127ad82aff01ff9aaf9939740b7722afaea
Tim Fenton <timothyfenton@outlook.com>
2024-12-10 20:41:35 -0500

    signin page style changes

--- app/(auth)/_components/facebook-auth-button.tsx
+++ app/(auth)/_components/facebook-auth-button.tsx
@@ -4,20 +4,23 @@ import { Icons } from '@/components/icons';
 
 export default function FacebookSignInButton() {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("facebook")
-      }}
-    >
-    <Button
-      className="w-full"
-      variant="outline"
-      type="submit"
-    >
-      <Icons.facebook fill='#000' strokeOpacity={0} className="mr-2 h-4 w-4" />
-      Continue with Facebook
-    </Button>
-    </form>
+    <div className='text-center'>
+      <form
+        className=''
+        action={async () => {
+          "use server"
+          await signIn("facebook")
+        }}
+      >
+      <Button
+        className="w-full max-w-[400px]"
+        variant="outline"
+        type="submit"
+      >
+        <Icons.facebook fill='#000' strokeOpacity={0} className="mr-2 h-4 w-4" />
+        Continue with Facebook
+      </Button>
+      </form>
+    </div>
   );
 }
--- app/(auth)/_components/sigin-view.tsx
+++ app/(auth)/_components/sigin-view.tsx
@@ -12,37 +12,29 @@ export const metadata: Metadata = {
 
 export default function SignInViewPage() {
   return (
-    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <Link
-        href="/examples/authentication"
-        className={cn(
-          buttonVariants({ variant: 'ghost' }),
-          'absolute right-4 top-4 hidden md:right-8 md:top-8'
-        )}
-      >
-        Login
-      </Link>
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+    <div className="relative h-screen flex-col items-center justify-center lg:max-w-none lg:grid lg:grid-cols-2 lg:px-0">
+      <div className="flex relative flex-row bg-muted p-6 gap-10 text-white dark:border-r w-full lg:flex-col lg:h-full lg:p-10 lg:items-start">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-medium">
           <ChaosLogo linkHref='/signin' />
         </div>
-        <div className="relative z-20 mt-auto">
-          <blockquote className="space-y-2">
-            <p className="text-lg">
-              Welcome to www.makinchaos.com! </p>
-            <p>Here you can claim your chaos or place a custom order. <br />Just signin with Facebook and submit the form.
-            </p>
-            <footer className="text-sm">- Trina</footer>
-          </blockquote>
+        <div className="relative z-20 mt-auto w-full lg:w-auto text-right lg:text-left">
+          
         </div>
       </div>
-      <div className="flex h-full items-center p-4 lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Sign in to order or claim
-            </h1>
+      <div className="flex h-auto md:h-full items-right p-4 lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] lg:w-[1000px] pt-20 lg:pt-0">
+          <div className="flex flex-col space-y-2 text-center mb-10">
+            <blockquote className="space-y-2">
+              <p className="text-2xl font-extrabold">
+                Welcome to makinchaos.com! 
+              </p>
+              <p className="text-xl">
+                Here to claim your chaos or order custom chaos? You've come to the right place,
+                just signin with Facebook and submit the form!
+              </p>
+              <footer className="text-sm">- Trina</footer>
+            </blockquote>
           </div>
           <UserAuthForm />
           <p className="px-8 text-center text-sm text-muted-foreground">
