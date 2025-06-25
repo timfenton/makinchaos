1e2f7e861c158650f22bf8e37b46d45d6738588e
Tim Fenton <timothyfenton@outlook.com>
2025-06-24 21:42:51 -0500

    fixing searching

--- components/ui/table/data-table-search.tsx
+++ components/ui/table/data-table-search.tsx
@@ -3,7 +3,8 @@
 import { Input } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
 import { Options } from 'nuqs';
-import { useTransition } from 'react';
+import { useEffect, useState, useTransition } from 'react';
+import { useDebounce } from '@/hooks/use-debounce';
 
 interface DataTableSearchProps {
   searchKey: string;
@@ -26,15 +27,23 @@ export function DataTableSearch({
 }: DataTableSearchProps) {
   const [isLoading, startTransition] = useTransition();
 
-  const handleSearch = (value: string) => {
-    setSearchQuery(value, { startTransition });
+  const [value, setValue] = useState<string | null>(searchQuery ?? '');
+
+  const debouncedValue = useDebounce(value, 300); // Debounce search input for 300ms
+
+  useEffect(() => {
+    setSearchQuery(debouncedValue, {startTransition});
     setPage(1); // Reset page to 1 when search changes
+  }, [debouncedValue, setPage, setSearchQuery]);
+
+  const handleSearch = (value: string) => {
+    setValue(value);
   };
 
   return (
     <Input
       placeholder={`Search ${searchKey}...`}
-      value={searchQuery ?? ''}
+      value={value ?? ''}
       onChange={(e) => handleSearch(e.target.value)}
       className={cn('w-full md:max-w-sm', isLoading && 'animate-pulse')}
     />
